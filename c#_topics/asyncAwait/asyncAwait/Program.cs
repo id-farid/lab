a5828fbe04b5082a5032662d063b1da0e78f3741
FaridIDBOURHIM <farid.idbourhim@hotmail.com>
2025-11-11 15:28:43 +0100

    addes async exercice

---
+++ c#_topics/asyncAwait/asyncAwait/Program.cs
@@ -0,0 +1,16 @@
+﻿async Task<string> OrderPizza()
+{
+    Console.WriteLine("Ordering pizza...");
+    await Task.Delay(5000);
+    return "Pizza delivered";
+}
+
+async Task MkeDinner()
+{ 
+      string status = await OrderPizza();
+      Console.WriteLine(status);
+      Console.WriteLine("Dinner is ready");
+}
+
+await MkeDinner();
+Console.WriteLine("Other tasks are running beside async call");
