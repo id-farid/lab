3e50a2ac6f750d6b04dbd1ae2bfb4fc05419590f
FaridIDBOURHIM <farid.idbourhim@hotmail.com>
2025-11-11 15:39:21 +0100

    Added delegate exercice

---
+++ c#_topics/delegate/delegate/Program.cs
@@ -0,0 +1,16 @@
+﻿
+public delegate void ActionDelegate(int number);
+
+public class Demo
+{
+    public static void CalculateSquare(int number)
+    {
+        Console.WriteLine(number * number);
+    }
+    
+    public static void Main()
+    {
+        ActionDelegate action = CalculateSquare;
+        action(5);
+    }
+}
